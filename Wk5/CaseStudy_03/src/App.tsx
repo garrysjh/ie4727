4db49b2e58fe15078b5229dc71f84788b51638f1
#GARRY SHI JIA HONG# <GSHI005@student.main.ntu.edu.sg>
2024-09-02 10:53:30 +0800

    Case Study 03

---
+++ Wk5/CaseStudy_03/src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import { Form } from './components/Form'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Here is the landing page with a fake form</h1>
+      <div className="card">
+        <Form/>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ Wk5/CaseStudy_03/src/components/Form.tsx
@@ -0,0 +1,76 @@
+import { useState } from "react"
+import { ChangeEvent, FormEvent } from "react"
+import '../styles/form.css'
+
+function Registered() {
+    const [registered, setRegistered] = useState({
+        registered: false
+    })
+    if (registered) {
+        return (
+            <>
+                Registration Successful
+            </>)
+    }
+    else {
+        return <></>
+    }
+}
+
+export function Form() {
+    const [formData, setFormData] = useState({
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    })
+    const changeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value })
+    }
+
+    const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        // Validation of username depends on conditions
+        if (formData['username'].length < 8) {
+            console.log("Your username is not longer than 8 characters")
+        }
+        // Validation of email is done in the input type. But we can specify special conditions with string formatting or regex
+        if (!formData['email'].includes('gmail')) {
+            console.log("Your email does not contain Gmail!");
+        }
+        // Validate that password has to be at least 8 characters, and confirm password is same as password
+        if (formData['password'].length < 8) {
+            alert("Your password has to be 8 characters or more.")
+        }
+        else if (formData['password'] != formData['confirmPassword']) {
+            alert("Your password is not the same as your confirmed password.")
+        }
+        else {
+            setRegistered(true)
+        }
+    }
+    return (
+        <div className="registrationForm">
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <label><h3>Username: </h3>
+                    <input type="text" name="username" placeholder="username" onChange={(e) => changeHandler(e)} />
+                </label>
+                <label><h3>Email: </h3>
+                    <input type="email" name="email" placeholder="example@gmail.com" onChange={(e) => changeHandler(e)} />
+                </label>
+                <label><h3>Password: </h3>
+                    <input type="password" name="password" placeholder="********" onChange={(e) => changeHandler(e)} />
+                </label>
+                <label><h3>Confirm Password: </h3>
+                    <input type="password" name="confirmPassword" placeholder="********" onChange={(e) => changeHandler(e)} />
+                </label>
+                <label>
+                    <h3>
+                        <input type="submit" value="Submit" />
+                    </h3>
+                </label>
+                <Registered />
+            </form>
+        </div>
+    )
+}
