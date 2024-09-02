5df5d1b62e2b101ad3651c5e4316653375913865
#GARRY SHI JIA HONG# <GSHI005@student.main.ntu.edu.sg>
2024-09-02 09:33:10 +0800

    modify php form actions

--- Wk4/CaseStudy_02/show_get.php
+++ Wk4/CaseStudy_02/show_get.php
@@ -1,3 +1,10 @@
-<?php
-  var_dump($_GET);
-?>
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?><br>
+Your experience is: <?php echo $_GET["experience"]; ?><br>
+Your start date is: <?php echo $_GET["startDate"]; ?><br>
+
+</body>
+</html>
