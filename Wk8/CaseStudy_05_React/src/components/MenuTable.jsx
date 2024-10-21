29ef53f3607acdc8e3b79549d772a484ee0517b1
#GARRY SHI JIA HONG# <GSHI005@student.main.ntu.edu.sg>
2024-10-21 10:52:16 +0800

    successfully retrieved from php

--- Wk8/CaseStudy_05_React/src/components/MenuTable.jsx
+++ Wk8/CaseStudy_05_React/src/components/MenuTable.jsx
@@ -1,30 +1,50 @@
 import { useState, useEffect } from 'react';
 
 export default function MenuTable() {
+    const [prices, setPrices] = useState({
+        
+            0: {
+                price: 5.00
+            },
+            1: {
+                price: 5.00
+            },
+            2: {
+                price: 5.00
+            },
+            3: {
+                price: 5.00
+            },
+            4: {
+                price: 5.00
+            },
+
+        
+    })
     const [menuItems, setMenuItems] = useState([
         {
             name: 'Just Java',
             description: 'Regular House blend, decaffeinated coffee, or flavor of the day. Endless Cup.',
-            price: 2.00,
+            price: prices[0].price,
             quantity: 0,
             subtotal: 0
         },
         {
             name: 'Cafe au Lait',
             description: 'House blended coffee infused into a smooth, steamed milk.',
-            price: 2.50,
+            price: prices[1].price,
             quantity: 0,
             subtotal: 0
         },
         {
             name: 'Iced Cappucino',
             description: 'Sweetened espresso blended with icy-cold milk and served in a chilled glass.',
-            price: 4.75,
+            price: prices[2].price,
             quantity: 0,
             subtotal: 0
         }
     ]);
-    const [prices, setPrices] = useState({})
+    
     useEffect(()=> {
         fetch('http://localhost:8000/gshi005/Documents/ie4727/Wk8/CaseStudy_05/menuGetPrice.php', {
             method: 'GET',
@@ -33,16 +53,15 @@ export default function MenuTable() {
               }
         })
         .then(response => {
-            if (!response.ok) {
-                throw new Error("No response, or error")
-            }
-            console.log(response.json())})
+            return response.json();
+        })
         .then(data => {
             console.log("Data Retrieved: ")
-            console.log(data)
-            setPrices(data)
+            console.log(data.prices)
+            console.log(data.prices[0]['price'])
+            setPrices(data.prices)
         })
-    }, [])
+    }, []);
 
     const updateQuantity = (index, quantity) => {
         const updatedMenuItems = [...menuItems];
