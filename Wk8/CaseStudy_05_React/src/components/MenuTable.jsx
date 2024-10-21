35cc86a2510e31dc52585270ffa8ab9fd2f424af
#GARRY SHI JIA HONG# <GSHI005@student.main.ntu.edu.sg>
2024-10-21 11:06:48 +0800

    add retrieval, now to calculate subtotal

--- Wk8/CaseStudy_05_React/src/components/MenuTable.jsx
+++ Wk8/CaseStudy_05_React/src/components/MenuTable.jsx
@@ -28,7 +28,7 @@ const MenuTable = () => {
     ]);
 
     useEffect(() => {
-        fetch('http://localhost/scripts/menuGetPrice.php')
+        fetch('http://localhost:8000/gshi005/Documents/ie4727/Wk8/CaseStudy_05/menuGetPrice.php')
             .then(response => {
                 if (!response.ok) {
                     throw new Error('Network response was not ok ' + response.statusText);
@@ -40,7 +40,33 @@ const MenuTable = () => {
                     throw new Error('Data is undefined');
                 }
                 console.log('Fetched data:', data); // Log the fetched data
-                setMenuItems(data);
+                console.log(data.prices[0]['price'])
+                const newPrices = [
+                    {
+                        name: 'Just Java',
+                        description: 'Regular House blend, decaffeinated coffee, or flavor of the day. Endless Cup.',
+                        price: Number(data.prices[0]['price']),
+                        quantity: 0,
+                        subtotal: 0
+                    },
+                    {
+                        name: 'Cafe au Lait',
+                        description: 'House blended coffee infused into a smooth, steamed milk.',
+                        priceOptions: [Number(data.prices[1]['price']), Number(data.prices[2]['price'])],
+                        price: 2.00,
+                        quantity: 0,
+                        subtotal: 0
+                    },
+                    {
+                        name: 'Iced Cappucino',
+                        description: 'Sweetened espresso blended with icy-cold milk and served in a chilled glass.',
+                        priceOptions: [Number(data.prices[3]['price']), Number(data.prices[4]['price'])],
+                        price: 4.75,
+                        quantity: 0,
+                        subtotal: 0
+                    }
+                ]
+                setMenuItems(newPrices)
             })
             .catch(error => console.error('Error fetching menu items:', error));
     }, []);
@@ -49,7 +75,7 @@ const MenuTable = () => {
         const updatedMenuItems = [...menuItems];
         updatedMenuItems[index].quantity = quantity;
         updatedMenuItems[index].subtotal = quantity * updatedMenuItems[index].price;
-        setMenuItems(updatedMenuItems);
+        setMenuItems([...updatedMenuItems]);
     };
 
     const calculateTotalPrice = () => {
@@ -90,7 +116,7 @@ const MenuTable = () => {
                                                     onChange={() => {
                                                         const updatedMenuItems = [...menuItems];
                                                         updatedMenuItems[index].price = priceOption;
-                                                        setMenuItems(updatedMenuItems);
+                                                        setMenuItems([...updatedMenuItems]);
                                                     }}
                                                 />
                                                 {priceOption === 2.00 || priceOption === 3.00 ? (
