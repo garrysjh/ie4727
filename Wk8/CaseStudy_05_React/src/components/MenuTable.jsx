0351a3b06d6e5b6cc80937281508612e6ef34efe
Garry <garrysjh@gmail.com>
2024-10-21 10:55:46 +0800

    try out new code

--- Wk8/CaseStudy_05_React/src/components/MenuTable.jsx
+++ Wk8/CaseStudy_05_React/src/components/MenuTable.jsx
@@ -1,66 +1,48 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export default function MenuTable() {
-    const [prices, setPrices] = useState({
-        
-            0: {
-                price: 5.00
-            },
-            1: {
-                price: 5.00
-            },
-            2: {
-                price: 5.00
-            },
-            3: {
-                price: 5.00
-            },
-            4: {
-                price: 5.00
-            },
-
-        
-    })
+const MenuTable = () => {
     const [menuItems, setMenuItems] = useState([
         {
             name: 'Just Java',
             description: 'Regular House blend, decaffeinated coffee, or flavor of the day. Endless Cup.',
-            price: prices[0].price,
+            price: 2.00,
             quantity: 0,
             subtotal: 0
         },
         {
             name: 'Cafe au Lait',
             description: 'House blended coffee infused into a smooth, steamed milk.',
-            price: prices[1].price,
+            priceOptions: [2.00, 3.00],
+            price: 2.00,
             quantity: 0,
             subtotal: 0
         },
         {
             name: 'Iced Cappucino',
             description: 'Sweetened espresso blended with icy-cold milk and served in a chilled glass.',
-            price: prices[2].price,
+            priceOptions: [4.75, 5.75],
+            price: 4.75,
             quantity: 0,
             subtotal: 0
         }
     ]);
-    
-    useEffect(()=> {
-        fetch('http://localhost:8000/gshi005/Documents/ie4727/Wk8/CaseStudy_05/menuGetPrice.php', {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json'
-              }
-        })
-        .then(response => {
-            return response.json();
-        })
-        .then(data => {
-            console.log("Data Retrieved: ")
-            console.log(data.prices)
-            console.log(data.prices[0]['price'])
-            setPrices(data.prices)
-        })
+
+    useEffect(() => {
+        fetch('http://localhost/scripts/menuGetPrice.php')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok ' + response.statusText);
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (!data) {
+                    throw new Error('Data is undefined');
+                }
+                console.log('Fetched data:', data); // Log the fetched data
+                setMenuItems(data);
+            })
+            .catch(error => console.error('Error fetching menu items:', error));
     }, []);
 
     const updateQuantity = (index, quantity) => {
@@ -68,7 +50,6 @@ export default function MenuTable() {
         updatedMenuItems[index].quantity = quantity;
         updatedMenuItems[index].subtotal = quantity * updatedMenuItems[index].price;
         setMenuItems(updatedMenuItems);
-        console.log(prices)
     };
 
     const calculateTotalPrice = () => {
@@ -84,7 +65,7 @@ export default function MenuTable() {
             <table>
                 <thead>
                     <tr>
-                        <th>Menu Item</th>
+                        <th>Item</th>
                         <th>Description</th>
                         <th>Price</th>
                         <th>Quantity</th>
@@ -97,71 +78,37 @@ export default function MenuTable() {
                             <td className="menu-item"><strong>{menuItem.name}</strong></td>
                             <td>{menuItem.description}</td>
                             <td>
-                                {menuItem.name === 'Cafe au Lait' ? (
+                                {menuItem.priceOptions ? (
                                     <div>
-                                        <input
-                                            type="radio"
-                                            name={`price-${index}`}
-                                            value={2.00}
-                                            checked={menuItem.price === 2.00}
-                                            onChange={() => {
-                                                const updatedMenuItems = [...menuItems];
-                                                updatedMenuItems[index].price = 2.00;
-                                                setMenuItems(updatedMenuItems);
-                                            }}
-                                        />
-                                        Single $2.00
-                                        <br />
-                                        <input
-                                            type="radio"
-                                            name={`price-${index}`}
-                                            value={3.00}
-                                            checked={menuItem.price === 3.00}
-                                            onChange={() => {
-                                                const updatedMenuItems = [...menuItems];
-                                                updatedMenuItems[index].price = 3.00;
-                                                setMenuItems(updatedMenuItems);
-                                            }}
-                                        />
-                                        Double $3.00
-                                    </div>
-                                ) : menuItem.name === 'Iced Cappucino' ? (
-                                    <div>
-                                        <input
-                                            type="radio"
-                                            name={`price-${index}`}
-                                            value={4.75}
-                                            checked={menuItem.price === 4.75}
-                                            onChange={() => {
-                                                const updatedMenuItems = [...menuItems];
-                                                updatedMenuItems[index].price = 4.75;
-                                                setMenuItems(updatedMenuItems);
-                                            }}
-                                        />
-                                        Single $4.75
-                                        <br />
-                                        <input
-                                            type="radio"
-                                            name={`price-${index}`}
-                                            value={5.75}
-                                            checked={menuItem.price === 5.75}
-                                            onChange={() => {
-                                                const updatedMenuItems = [...menuItems];
-                                                updatedMenuItems[index].price = 5.75;
-                                                setMenuItems(updatedMenuItems);
-                                            }}
-                                        />
-                                        Double $5.75
+                                        {menuItem.priceOptions.map((priceOption, priceIndex) => (
+                                            <div key={priceIndex}>
+                                                <input
+                                                    type="radio"
+                                                    name={`price-${index}`}
+                                                    value={priceOption}
+                                                    checked={menuItem.price === priceOption}
+                                                    onChange={() => {
+                                                        const updatedMenuItems = [...menuItems];
+                                                        updatedMenuItems[index].price = priceOption;
+                                                        setMenuItems(updatedMenuItems);
+                                                    }}
+                                                />
+                                                {priceOption === 2.00 || priceOption === 3.00 ? (
+                                                    `Single $${priceOption.toFixed(2)}`
+                                                ) : (
+                                                    `Double $${priceOption.toFixed(2)}`
+                                                )}
+                                                <br />
+                                            </div>
+                                        ))}
                                     </div>
                                 ) : (
                                     `$${menuItem.price.toFixed(2)}`
                                 )}
-                                
                             </td>
                             <td>
                                 <input
                                     type="number"
-                                    min="0"
                                     value={menuItem.quantity}
                                     onChange={(e) => updateQuantity(index, parseInt(e.target.value))}
                                 />
@@ -171,8 +118,9 @@ export default function MenuTable() {
                     ))}
                 </tbody>
             </table>
-
-            <p className="output">Total Price: ${calculateTotalPrice().toFixed(2)}</p>
+            <h2>Total Price: ${calculateTotalPrice().toFixed(2)}</h2>
         </div>
     );
-}
+};
+
+export default MenuTable;
